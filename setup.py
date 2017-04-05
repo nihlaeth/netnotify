bc0f1d5ba62dd480e19e58a47a579e7539ec1ab5
nihlaeth <info@nihlaeth.nl>
2017-04-05 21:51:55 +0200

    update requirements: make server an extra

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 """Installation script for netnotify."""
-# pylint: disable=wrong-import-position
-import sys
-from setuptools import setup, find_packages, Command
+from setuptools import setup
 
 with open('README.rst', 'r') as readme:
     LONG_DESCRIPTION = readme.read()
@@ -16,8 +14,9 @@ setup(
     license='GPLv3',
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*',
     scripts=['netnotify.py'],
-    install_requires=[
-        'dbus-python', 'notify2', 'user_config>=1.0a10', 'daemonocle'],
+    install_requires=['user_config>=1.0a10'],
+    extras_require={
+        'server': ['notify2', 'daemonocle']},
     entry_points={
         'console_scripts': [
             'netnotify = netnotify:send',
